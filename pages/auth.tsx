843fa8e8f4544d625c0918ff946f0d39090d0741
SErgioNbP <sergionevespinheiro@gmail.com>
2023-03-27 14:29:25 +0100

    Navbar and account menu created

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,15 +2,13 @@ import axios from 'axios';
 import { useCallback, useState } from 'react';
 import Input from '@/components/Input';
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/router';
 
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
-  const router = useRouter();
   const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
+  const [name, setname] = useState('');
   const [password, setPassword] = useState('');
 
   const [variant, setVariant] = useState('login');
@@ -26,21 +24,18 @@ const Auth = () => {
       await signIn('credentials', {
         email,
         password,
-        redirect: false,
-        callbackUrl: '/',
+        callbackUrl: '/profiles',
       });
-
-      router.push('/');
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
       await axios.post('/api/register', {
         email,
-        username,
+        name,
         password,
       });
 
@@ -48,7 +43,7 @@ const Auth = () => {
     } catch (error) {
       console.log(error);
     }
-  }, [email, username, password, login]);
+  }, [email, name, password, login]);
 
   return (
     <div
@@ -71,9 +66,9 @@ const Auth = () => {
               {variant === 'register' && (
                 <Input
                   label="Username"
-                  onChange={(e: any) => setUsername(e.target.value)}
-                  id="username"
-                  value={username}
+                  onChange={(e: any) => setname(e.target.value)}
+                  id="name"
+                  value={name}
                 />
               )}
               <Input
@@ -103,18 +98,20 @@ const Auth = () => {
             justify-center"
             >
               <div
+                onClick={() => signIn('google', { callbackUrl: 'profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex 
                 items-center justify-center cursor-pointer hover:opacity-80
                 transition"
               >
-                <FcGoogle size={30} />
+                <FcGoogle size={32} />
               </div>
               <div
+                onClick={() => signIn('github', { callbackUrl: '/profiles' })}
                 className="w-10 h-10 bg-white rounded-full flex 
                 items-center justify-center cursor-pointer hover:opacity-80
                 transition"
               >
-                <FaGithub size={30} />
+                <FaGithub size={32} />
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
